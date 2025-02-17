4706b2aa5b6759007a19d9f22006bbece3443b97
mujtabasaqib <mujtabasaqib99@gmail.com>
2025-02-17 20:57:35 +0500

    initialized repo

--- anchor/programs/basic/src/lib.rs
+++ anchor/programs/basic/src/lib.rs
@@ -3,14 +3,59 @@ use anchor_lang::prelude::*;
 declare_id!("6z68wfurCMYkZG51s1Et9BJEd9nJGUusjHXNt4dGbNNF");
 
 #[program]
-pub mod basic {
+pub mod token_lottery {
     use super::*;
 
-    pub fn greet(_ctx: Context<Initialize>) -> Result<()> {
-        msg!("GM!");
+    pub fn initialize_config(ctx: Context<InitializeConifg>, start: u64, end: u64, price: u64) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn initialize_lottery(ctx: Context<InitializeLottery>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn buy_ticket(ctx: Context<BuyTicket>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn commit_a_winner(ctx: Context<CommitWinner>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn choose_a_winner(ctx: Context<ChooseWinner>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn claim_prize(ctx: Context<ClaimPrize>) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeConifg<'info> {
+}
+
+#[derive(Accounts)]
+pub struct ClaimPrize<'info> {
+}
+
+#[derive(Accounts)]
+pub struct CommitWinner<'info> {
+}
+
+#[derive(Accounts)]
+pub struct ChooseWinner<'info> {
+}
+
+#[derive(Accounts)]
+pub struct BuyTicket<'info> {
+}
+
+#[derive(Accounts)]
+pub struct InitializeLottery<'info> {
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct TokenLottery {
+}
